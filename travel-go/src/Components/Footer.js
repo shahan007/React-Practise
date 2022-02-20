ce4bc5197a3c984f58c121513638aed2fa926033
shahan alam <shahanisright@gmail.com>
2022-02-20 20:18:51 +0800

    travel-go: updated Footer component, added target attribute for a tags

--- travel-go/src/Components/Footer.js
+++ travel-go/src/Components/Footer.js
@@ -2,10 +2,10 @@ const githubLink = "https://github.com/shahan007"
 const linkedinLink  = "https://www.linkedin.com/in/shahan-alam-71a161206/"
 const Footer = ()=>(
     <footer> 
-        <a href={linkedinLink}>
+        <a href={linkedinLink} target="_blank" rel='noreferrer'>
             <i className="fa-brands fa-linkedin"></i>
         </a>        
-        <a href={githubLink}>
+        <a href={githubLink} target="_blank" rel='noreferrer'>
             <i className="fa-brands fa-github-square"/>
         </a>        
     </footer>
